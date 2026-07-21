97e7e9e9d9a0cf2745c257206e98c5370fe592e0
Christopher Mayfield <cmayfield@users.noreply.github.com>
2026-07-21 16:57:53 -0500

    feat: display hello world on CYD

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,26 @@
 #include <Arduino.h>
+#include <TFT_eSPI.h>
+
+namespace {
+constexpr uint8_t kBacklightPin = 21;
+TFT_eSPI display;
+}
 
 void setup() {
     Serial.begin(115200);
-    Serial.println("Flip7 CYD Scorekeeper");
+
+    pinMode(kBacklightPin, OUTPUT);
+    digitalWrite(kBacklightPin, HIGH);
+
+    display.init();
+    display.setRotation(1);
+    display.fillScreen(TFT_NAVY);
+    display.setTextColor(TFT_WHITE, TFT_NAVY);
+    display.setTextDatum(MC_DATUM);
+    display.drawString("Hello, World!", display.width() / 2,
+                       display.height() / 2, 4);
+
+    Serial.println("Hello, World! displayed on CYD");
 }
 
 void loop() {
